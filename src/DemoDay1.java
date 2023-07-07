3607039d0465b0b9e99523864b7e1e7a3fce5e7c
Akarsh <akkikarn20@gmail.com>
2023-07-07 15:07:18 +0530

    DemoDay1

---
+++ src/DemoDay1.java
@@ -0,0 +1,135 @@
+public class DemoDay1 {
+    public static void main(String[] args) {
+
+        System.out.println("Assignment 01 : 7-7-23 : Write 10 values of each single primitivve datatype and print all the data.");
+        System.out.println("\n");
+
+        System.out.println("Following are the different Primitive DataTypes variables:");
+        System.out.println("\n");
+
+        // 1) Int variable
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = 50;
+        int f = 60;
+        int g = 70;
+        int h = 80;
+        int i = 90;
+        int j = 100;
+
+        System.out.println("The following are the values of different int variables :" + "\n" + a + " "+ b + " "+ c + " " + d + " "+ e + " "+ f + " "+ g + " "+ h + " "+ i + " "+ j );
+        System.out.println("\n");
+
+        // 2) Double variable
+
+        double k = 10.11111;
+        double l = 11.11111;
+        double m = 12.11111;
+        double n = 13.11111;
+        double o = 14.11111;
+        double p = 15.11111;
+        double q = 16.11111;
+        double r = 17.11111;
+        double s = 18.11111;
+        double t = 19.11111;
+
+        System.out.println("The following are the values of different double variables :" + "\n" + k + " "+ l + " "+ m + " " + n + " "+ o + " "+ p + " "+ q + " "+ r + " "+ s + " "+ t );
+        System.out.println("\n");
+
+        // 3) Float variable
+
+        float aa = 1.1f;
+        float ab = 1.1f;
+        float ac = 1.1f;
+        float ad = 1.1f;
+        float ae = 1.1f;
+        float af = 1.1f;
+        float ag = 1.1f;
+        float ah = 1.1f;
+        float ai = 1.1f;
+        float aj = 1.1f;
+
+        System.out.println("The following are the values of different float variables :" + "\n" + aa + " "+ ab + " "+ ac + " " + ad + " "+ ae + " "+ af + " "+ ag + " "+ ah + " "+ ai + " "+ aj );
+        System.out.println("\n");
+
+        // 4) byte Variable
+
+        byte ak = 0;
+        byte al = 1;
+        byte am = 2;
+        byte an = 3;
+        byte ao = 4;
+        byte ap = 5;
+        byte aq = 6;
+        byte ar = 7;
+        byte as = 8;
+        byte at = 9;
+
+        System.out.println("The following are the values of different byte variables :" + "\n" + ak + " "+ al + " "+ am + " " + an + " "+ ao + " "+ ap + " "+ aq + " "+ ar + " "+ as + " "+ at );
+        System.out.println("\n");
+
+        // 5) short Variable
+
+        short ba = 11;
+        short bb = 12;
+        short bc = 13;
+        short bd = 14;
+        short be = 15;
+        short bf = 16;
+        short bg = 17;
+        short bh = 18;
+        short bi = 19;
+        short bj = 21;
+
+        System.out.println("The following are the values of different short variables :" + "\n" + ba + " "+ bb + " "+ bc + " " + bd + " "+ be + " "+ bf + " "+ bg + " "+ bh + " "+ bi + " "+ bj );
+        System.out.println("\n");
+
+        // 6) long Variable
+
+        long bk = 1000;
+        long bl = 2000;
+        long bm = 3000;
+        long bn = 4000;
+        long bo = 5000;
+        long bp = 6000;
+        long bq = 7000;
+        long br = 8000;
+        long bs = 9000;
+        long bt = 10000;
+
+        System.out.println("The following are the values of different long variables :" + "\n" + bk + " "+ bl + " "+ bm + " " + bn + " "+ bo + " "+ bp + " "+ bq + " "+ br + " "+ bs + " "+ bt );
+        System.out.println("\n");
+
+        // 7) char variable
+
+        char ca = '1';
+        char cb = 'a';
+        char cc = '5';
+        char cd = '7';
+        char ce = 'd';
+        char cf = 'x';
+        char cg = 'u';
+        char ch = 'v';
+        char ci = 'w';
+        char cj = 'z';
+
+        System.out.println("The following are the values of different char variables :" + "\n");
+        System.out.println(ca);
+        System.out.println(cb);
+        System.out.println(cc);
+        System.out.println(cd);
+        System.out.println(ce);
+        System.out.println(cf);
+        System.out.println(cg);
+        System.out.println(ch);
+        System.out.println(ci);
+        System.out.println(cj);
+
+        System.out.println("\n");
+
+        System.out.println("End of program.");
+        System.out.println("Thank You.");
+    }
+}
