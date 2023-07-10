c241107286788e796e0f250ba25e15dc8b5ba474
Akarsh <akkikarn20@gmail.com>
2023-07-10 09:16:54 +0530

    DemoCalculatot

--- src/DemoCalculator.java
+++ src/DemoCalculator.java
@@ -9,6 +9,18 @@ public class DemoCalculator {
         System.out.println("Please enter your second input");
         int b = sc.nextInt();
 
+        System.out.println("Enter \n 1: Addition \n 2: Subtraction \n 3: Multiplication \n 4: Division");
+        int output = sc.nextInt();
+
+        switch (output){
+            case 1-> System.out.println("Addition:" + (a+b));
+            case 2-> System.out.println("Addition:" + (a-b));
+            case 3-> System.out.println("Addition:" + (a*b));
+            case 4-> System.out.println("Addition:" + (a/b));
+        }
+
+
+
 
        }
        }
