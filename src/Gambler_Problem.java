3dd0edda0b5d4f630a9f4e990f9879e96fddb45c
Akarsh <akkikarn20@gmail.com>
2023-07-12 09:01:49 +0530

    Gambler_Problem

---
+++ src/Gambler_Problem.java
@@ -0,0 +1,32 @@
+// Ques 1) Write a program and take 10 input from user and store it in an array, then display the array and sort the array and again display the array.
+
+import java.util.Scanner;
+
+public class Gambler_Problem {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        int ar[] = new int[a];
+
+        System.out.println("Enter the input of the array");
+        for (int i = 0; i < a; i++) {
+            ar[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < a - 1; i++) {
+            for (int j = i+1; j<a -1; j++) {
+                if (ar[j] < ar[j + 1]) {
+                    int x = ar[j];
+                    ar[j] = ar[j + 1];
+                    ar[j + 1] = x;
+                }
+            }
+        }
+        System.out.println("Displaying the sorted array");
+        for (int i = 0; i < a; i++) {
+            System.out.println(ar[i] + " ");
+        }
+
+    }
+}
